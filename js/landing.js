a836a29e8419194d3bdf4dcda7b90e1e5fc6521d
Firoza Merxat <firozauxui@gmail.com>
2022-08-04 18:39:04 -0400

    changes

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,16 @@
+/*__________________Text hover effect using jQuery___________________*/
+$("loading_box li a").on("hover"),function(){
+    if(){
+        $(this).css("text-decoration","underline")};
+
+    else(){
+        $(this).css("text-decoration","none")
+    };
+};
+
+
+/*__________________Typing animation using js________________________*/
+
 var i = 0, text; /*setting the initial position*/
 
 text = 'Bello, my name is Firoza. Welcome to my zone!';
@@ -9,7 +22,10 @@ function typing(){
         document.getElementById('text').innerHTML += text.charAt(i);
         i++;
         setTimeout(typing,100); /*setting transition time*/
-    }
+    } 
 }
 typing();
+
+
+
 
