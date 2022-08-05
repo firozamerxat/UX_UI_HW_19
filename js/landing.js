3f50cbda2f3d13dc0004ac0a7c4b027c42d17010
Firoza Merxat <firozauxui@gmail.com>
2022-08-05 14:15:11 -0400

    changes

--- js/landing.js
+++ js/landing.js
@@ -1,51 +1,20 @@
-/*__________________Text hover effect using jQuery___________________
-
-method 1 (dit not work)
-
-$("landing_box li a").on("hover",function(){
-    if(true){
-        $(this).css("text-decoration-line","underline")
-    }
-    
-    else{
-        $(this).css("text-decoration-line","none")
-    }
-});
+/*__________________Text hover effect using jQuery___________________*/
 
 
-method 2 (did not work)
-
-$(landing_box li a).ready(function(){
-    $("landing_box li a").hover(function(){
-        $(this).css("text-decoration","underline");
-    },function(){
-        $(this).css("text-decoration","none");
-    });
-});
-
-
-
-method 3 (did not work)
-$(apple).hover(function () {
-    $(this).css("text-decoration","underline");
-    // over
-            
-    }, function () {
-   
+$(".landingBox").hover(
+    function() {
+      $(this).addClass("applyUnderline"); console.log("MouseEnter");
+    }, function() {
+      $(this).removeClass("applyUnderline");console.log("MouseLeave");
     }
 );
-*/
 
 
-
-/*__________________Typing animation using js________________________*/
-
-var i = 0, text; /*setting the initial position*/
-
+/*_____________creating function for the typing effect___________*/
+var i = -1, text; /*setting the initial position*/
 text = 'Bello, my name is Firoza. Welcome to my zone!';
 
 
-/*creating function for the typing effect*/
 function typing(){
     if(i<text.length){
         document.getElementById('text').innerHTML += text.charAt(i);
